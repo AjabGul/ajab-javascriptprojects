afac49e1a0feb7ca399753cf46b8fea09ee8d512
Ajab Gul <kakar.ajabgul@gmail.com>
2020-12-14 22:06:53 +0500

    update project 17

--- P_17/script.js
+++ P_17/script.js
@@ -1,11 +1,7 @@
-const list = document.getElementById('list');
+const dragbox = document.getElementById('sortlist');
 const checkBtn = document.getElementById('btn');
+const listIndex = document.getElementById('listLi');
 
-// sortable box 
-var dragBox = document.querySelector('.dragbox');
-new Sortable(dragBox, {
-    animation: 400
-})
 
 const largestCountries = [
 
@@ -23,6 +19,9 @@ const largestCountries = [
 
 
 let countryList = [];
+
+// Fishier algorithum
+
 for (var i = 0; i < largestCountries.length; i++) {
     countryList.push(largestCountries[i]);
 }
@@ -35,25 +34,106 @@ var i = countryList.length, j, temp;
         countryList[i] = temp;
     }
 
-console.log(largestCountries);
-console.log(countryList);
+// console.log(largestCountries);
+// console.log(countryList);
 
 countryList.forEach(addToListTtem);
 
 
-// functions 
+// Functions 
 
 function addToListTtem(country, index) {
 
-    list.innerHTML += `
-         <li draggable="True"> <span>${index + 1}</span> ${country} <br/> <i class="fas fa-grip-lines"></i></li>
-         
+    dragbox.innerHTML += `
+
+    <li draggable="true" id="listLi"> <span>${index + 1}</span> ${country} <br/> <i class="fas fa-grip-lines"></i></li>
+
+
   `;
 
 }
 
 
-// EventListener
-checkBtn.addEventListener('click', ()=>{
+// drag and drop starts here
+
+
+// wait for window to fully loaded
+window.addEventListener('load', function(){
+    // get all the item 
+    var items = document.querySelectorAll('#sortlist li'),
+        // create a empty varible to hold dragg element
+        dragged = null;
+    // now create 7 draggable events to drage the list
+    for(let i of items){
+        // event 1 the list not dragged add hint color = yellow
+        i.addEventListener("dragstart", function(){
+            dragged = this;
+            for(let it of items){
+                if(it != dragged){ it.classList.add("hint");}
+            }
+            
+        });
+
+        // event 2 when draged give different highlight color
+        i.addEventListener("dragenter", function(){
+            if (this != dragged){ this.classList.add("active");}
+        });
+
+
+        // event 3 when draged leave removie active color(pink)
+        i.addEventListener("dragleave", function(){
+            this.classList.remove("active");
+        });
+
+
+        // event 4 when the draged end give remove all color for all list
+        i.addEventListener("dragend", function(){
+            for(let it of items){
+                it.classList.remove("hint");  
+                it.classList.remove("active");
+            }
+        });
+
+        // NOT IN USE
+
+
+        // event 5:
+        i.addEventListener("dragover", function(evt){
+           evt.preventDefault();
+        });
+
+
+        // event 6:
+        i.addEventListener("drop", function(evt){
+            evt.preventDefault();
+            if(this !=dragged){
+                let all = document.querySelectorAll(".sortlist li"),
+                    draggedpos = 0, droppedpos = 0;
+                for(let it=0; it<all.length; it++){
+                    if (dragged == all[it]){draggedpos = it;}
+                    if (this == all[it]){droppedpos = it;}
+                }
+                if(draggedpos < droppedpos){
+                    this.parentNode.insertBefore(dragged, this.nextSibling);
+                }else{
+                    this.parentNode.insertBefore(dragged, this);
+                }
+                
+            }
+         });
+
+    }
+});
+
+// drag and drop ends here
     
-})
+
+
+
+
+// // EventListener
+// checkBtn.addEventListener('click', ()=>{
+//     var displaytext = listIndex.option.value;
+//     country.value =displaytext;
+//     console.log(displaytext);
+// });
